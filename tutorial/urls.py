39f0ca822541e655b26541e9b12716bbc888b3f0
kyungjejo <kyungjejo@gmail.com>
2017-12-21 14:12:04 +0900

    demo error fixed

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('account.urls')),
-    url(r'^demo/', include('demo.urls')),
+    # url(r'^demo/', include('demo.urls')),
 ]
