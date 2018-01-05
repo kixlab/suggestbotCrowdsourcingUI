340e02e39c857c9851b900e3002509a8f529e857
johnr0 <johnr0hol@gmail.com>
2018-01-05 11:43:25 +0900

    progress bar time change

--- account/static/account/js/interactive_progress_bar.js
+++ account/static/account/js/interactive_progress_bar.js
@@ -1,32 +1,32 @@
 
 
 // Get a handle to the player
-player       = document.getElementById('myVideo');
+vd_player       = document.getElementById('myVideo');
 btnPlayPause = document.getElementById('btnPlayPause');
 progressBar  = document.getElementById('progress');
 
 // Add a listener for the timeupdate event so we can update the progress bar
-player.addEventListener('timeupdate', updateProgressBar, false);
+vd_player.addEventListener('timeupdate', updateProgressBar, false);
 
 // Add a listener for the play and pause events so the buttons state can be updated
-player.addEventListener('play', function() {
+vd_player.addEventListener('play', function() {
   // Change the button to be a pause button
   changeButtonType(btnPlayPause, 'pause');
 }, false);
 
-player.addEventListener('pause', function() {
+vd_player.addEventListener('pause', function() {
   // Change the button to be a play button
   changeButtonType(btnPlayPause, 'play');
 }, false);
 
-player.addEventListener('ended', function() { this.pause(); }, false);
+vd_player.addEventListener('ended', function() { this.pause(); }, false);
 
 progressBar.addEventListener("click", seek);
 
 function seek(e) {
   var elem = document.getElementById("progress-bar");
   var percent = e.offsetX / this.offsetWidth;
-  player.currentTime = percent * player.duration;
+  vd_player.currentTime = percent * vd_player.duration;
 
   $('#pr-bar-tooltip').tooltip('show');
 
@@ -35,17 +35,17 @@ function seek(e) {
 }
 
 function playPauseVideo() {
-  if (player.paused || player.ended) {
+  if (vd_player.paused || vd_player.ended) {
     // Change the button to a pause button
     changeButtonType(btnPlayPause, 'pause');
     document.getElementById('playpauseimg').src="../../static/account/img/icon_round_pause.png";
-    player.play();
+    vd_player.play();
   }
   else {
     // Change the button to a play button
     changeButtonType(btnPlayPause, 'play');
     document.getElementById('playpauseimg').src="../../static/account/img/icon_round_play.png";
-    player.pause();
+    vd_player.pause();
   }
 }
 
@@ -53,7 +53,7 @@ function playPauseVideo() {
 function updateProgressBar() {
   // Work out how much of the media has played via the duration and currentTime parameters
   var elem = document.getElementById("progress-bar");
-  var percentage = Math.floor((100 / player.duration) * player.currentTime);
+  var percentage = Math.floor((100 / vd_player.duration) * vd_player.currentTime);
 
   $('#pr-bar-tooltip').tooltip('show');
   // Update the progress bar's value
@@ -77,7 +77,7 @@ function enable_tagging() {
   // Change the button to a play button
   changeButtonType(btnPlayPause, 'play');
   document.getElementById('playpauseimg').src="../../static/account/img/icon_round_play.png";
-  player.pause();
+  vd_player.pause();
 }
 
 $(document).ready(function(){
@@ -94,7 +94,7 @@ $(document).ready(function(){
     // Change the button to a pause button
     changeButtonType(btnPlayPause, 'pause');
     document.getElementById('playpauseimg').src="../../static/account/img/icon_round_pause.png";
-    player.play();
+    vd_player.play();
   });
-  
+
 });
