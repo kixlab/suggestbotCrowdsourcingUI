41c9a2d578cfb0f57ad201900f6f398000d9d3c2
johnr0 <johnr0hol@gmail.com>
2018-01-14 22:57:19 +0900

    feedback changing

--- account/static/account/js/mturk.js
+++ account/static/account/js/mturk.js
@@ -115,7 +115,7 @@ function feedbackSubmit() { // added by Arti
     type: "POST",
     data: feedbackdataPackage,
     success: function(d) {
-      $("#mturk_form").submit();
+      //$("#mturk_form").submit();
       alert("Thank you for submitting the task! Your HIT is being processed and evaluated as part of a quality check. You'll be paid soon.");
       console.log("succeeded saving worker's feedback to db");
     }
